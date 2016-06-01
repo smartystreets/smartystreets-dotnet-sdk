47d99495eb2e05e176e135b877372ea49994eace
Oshion Niemela <oshion@smartystreets.com>
2016-06-01 10:32:34 -0600

    Preparing nunit.

---
+++ src/sdk/USStreetApi/Client.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SmartyStreets
+{
+	public class Client
+	{
+	}
+}
---
+++ src/tests/USStreetApi/ClientTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SmartyStreets
+{
+	public class ClientTests
+	{
+		public ClientTests ()
+		{
+		}
+
+	}
+}
+
