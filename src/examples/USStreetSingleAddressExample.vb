91a5a23a07d3ee61ce0fce270d7dafa0abb4cfb3
Whatcott, Michael <mdwhatcott@gmail.com>
2016-10-20 12:14:51 -0600

    Included vb.net example (not part of the sample project).

---
+++ src/examples/USStreetSingleAddressExample.vb
@@ -0,0 +1,52 @@
+Imports System
+Imports System.IO
+Imports SmartyStreets
+Imports SmartyStreets.USStreetApi
+ 
+Module Module1
+ 
+    Dim authID = Environment.GetEnvironmentVariable("SMARTY_AUTH_ID")
+    Dim authToken = Environment.GetEnvironmentVariable("SMARTY_AUTH_TOKEN")
+ 
+    Dim client = New ClientBuilder(authID, authToken).Build()
+ 
+    Sub Test_SingleAddress()
+ 
+        Dim myLookup As New Lookup()
+        With myLookup
+            .Street = "1600 Amphitheatre Pkwy"
+            .City = "Mountain View"
+            .State = "CA"
+        End With
+ 
+        Try
+            client.Send(myLookup)
+        Catch ex As SmartyException
+            Console.WriteLine(ex.Message)
+            Console.WriteLine(ex.StackTrace)
+        Catch ex As IOException
+            Console.WriteLine(ex.StackTrace)
+        End Try
+ 
+        Dim candidates = myLookup.Result
+ 
+        If candidates.Count = 0 Then
+            Console.WriteLine("No candidates. This means the address is not valid.")
+            Return
+        End If
+ 
+        Dim firstCandidate = candidates(0)
+ 
+        Console.WriteLine("Address is valid. (There is at least one candidate)" & vbLf)
+        Console.WriteLine("Primary Number: " + firstCandidate.Components.PrimaryNumber)
+        Console.WriteLine("Street: " + firstCandidate.Components.StreetName)
+        Console.WriteLine("CityName: " + firstCandidate.Components.CityName)
+        Console.WriteLine("State: " + firstCandidate.Components.State)
+        Console.WriteLine("ZIP Code: " + firstCandidate.Components.ZipCode)
+        Console.WriteLine("County: " + firstCandidate.Metadata.CountyName)
+        Console.WriteLine("Latitude: " + firstCandidate.Metadata.Latitude.ToString)
+        Console.WriteLine("Longitude: " + firstCandidate.Metadata.Longitude.ToString)
+ 
+    End Sub
+ 
+End Module
