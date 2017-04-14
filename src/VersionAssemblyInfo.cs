2fc9080b46e7a76564f95f33cb0ca1bbe1593ef7
Whatcott, Michael <mdwhatcott@gmail.com>
2017-04-14 08:53:38 -0600

    Comment: Version is updated by build job.

--- src/VersionAssemblyInfo.cs
+++ src/VersionAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyVersion("0.0.0")] // DO NOT EDIT (this is updated by a build job when a new release is published)
+[assembly: AssemblyFileVersion("0.0.0")] // DO NOT EDIT (this is updated by a build job when a new release is published)
