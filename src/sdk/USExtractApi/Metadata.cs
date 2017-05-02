76957921d93b4269935e8032784aa87d8f72ddf4
Neo Pimentel <neo@smartystreets.com>
2017-05-02 11:18:01 -0600

    Implemented extract’s Metadata.

---
+++ src/sdk/USExtractApi/Metadata.cs
@@ -0,0 +1,37 @@
+﻿namespace SmartyStreets.USExtractApi
+{
+	using System.Runtime.Serialization;
+
+	/// <summary>
+	/// See "https://smartystreets.com/docs/cloud/us-extract-api#http-response-status"
+	/// </summary>
+	[DataContract]
+	public class Metadata
+	{
+		#region [ Fields ]
+
+		[DataMember(Name = "lines")]
+		public int lines { get; private set; }
+
+		[DataMember(Name = "unicode")]
+		public bool unicode { get; private set; }
+
+		[DataMember(Name = "address_count")]
+		public int addressCount { get; private set; }
+
+		[DataMember(Name = "verified_count")]
+		public int verifiedCount { get; private set; }
+
+		[DataMember(Name = "bytes")]
+		public int bytes { get; private set; }
+
+		[DataMember(Name = "character_count")]
+		public int characterCount { get; private set; }
+
+		#endregion
+
+		public Metadata()
+		{
+		}
+	}
+}
