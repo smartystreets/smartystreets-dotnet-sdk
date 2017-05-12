42f440db947f8dbd54d1e18e22cb6996a0667c42
Neo Pimentel <neo@smartystreets.com>
2017-05-12 15:52:46 -0600

    Implemented Suggestion.

--- src/sdk/USAutocompleteApi/Suggestion.cs
+++ src/sdk/USAutocompleteApi/Suggestion.cs
@@ -1,7 +1,23 @@
-﻿namespace SmartyStreets.USAutocompleteApi
+﻿using System.Runtime.Serialization;
+
+namespace SmartyStreets.USAutocompleteApi
 {
+    /// <remarks>
+    /// See "https://smartystreets.com/docs/cloud/us-autocomplete-api#http-response"
+    /// </remarks>>
+    [DataContract]
     public class Suggestion
     {
+        [DataMember(Name = "text")]
+        public string Text { get; }
 
+        [DataMember(Name = "street_line")]
+        public string StreetLine { get; }
+
+        [DataMember(Name = "city")]
+        public string City { get; }
+
+        [DataMember(Name = "state")]
+        public string State { get; }
     }
 }
