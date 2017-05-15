a8412af58f0cb6cdde30ee28dcd451c970bc9930
Neo Pimentel <neo@smartystreets.com>
2017-05-15 15:08:07 -0600

    Implemented Autocomplete’s Client.

---
+++ src/sdk/USAutocompleteApi/Client.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.IO;
+
+namespace SmartyStreets.USAutocompleteApi
+{
+    /// <summary>
+    /// This client sends lookups to the SmartyStreets US Autocomplete API,
+    /// and attaches the results to the appropriate Lookup objects.
+    /// </summary>
+    public class Client
+    {
+        private ISender sender;
+        private ISerializer serializer;
+
+        public Client(ISender sender, ISerializer serializer)
+        {
+            this.sender = sender;
+            this.serializer = serializer;
+        }
+
+        public Suggestion[] Send(Lookup lookup)
+        {
+            if (string.IsNullOrEmpty(lookup?.Prefix))
+                throw new SmartyException("Send() must be passed a Lookup with the prefix field set.");
+
+            var request = BuildRequest(lookup);
+
+            var response = this.sender.Send(request);
+
+            using (var payloadStream = new MemoryStream(response.Payload))
+            {
+                var result = this.serializer.Deserialize<Result>(payloadStream) ?? new Result();
+                var suggestions = result.Suggestions;
+                lookup.Result = suggestions;
+                return suggestions;
+            }
+        }
+
+        private Request BuildRequest(Lookup lookup)
+        {
+            var request = new Request();
+
+            request.SetParameter("prefix", lookup.Prefix);
+            request.SetParameter("suggestions", lookup.MaxSuggestions.ToString());
+            request.SetParameter("city_filter", BuildFilterString(lookup.CityFilter));
+            request.SetParameter("state_filter", BuildFilterString(lookup.StateFilter));
+            request.SetParameter("prefer", BuildFilterString(lookup.Prefer));
+
+            if (lookup.GeolocateType != GeolocateType.NONE)
+            {
+                request.SetParameter("geolocate", "true");
+                request.SetParameter("geolocate_precision", lookup.GeolocateType);
+            }
+            else request.SetParameter("geolocate", "false");
+
+            return request;
+        }
+
+        private string BuildFilterString(ArrayList list)
+        {
+            if (list.Count == 0)
+                return null;
+
+            var filterList = "";
+
+            foreach (string item in list)
+                filterList += (item + ",");
+
+            if (filterList.EndsWith(","))
+                filterList = filterList.Substring(0, filterList.Length - 1);
+
+            return filterList;
+        }
+    }
+}
---
+++ src/tests/USAutocompleteApi/ClientTests.cs
@@ -0,0 +1,115 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace SmartyStreets.USAutocompleteApi
+{
+    [TestFixture]
+    public class ClientTests
+    {
+        private RequestCapturingSender capturingSender;
+        private URLPrefixSender urlSender;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.capturingSender = new RequestCapturingSender();
+            this.urlSender = new URLPrefixSender("http://localhost/", this.capturingSender);
+        }
+
+        #region [ Single Lookup ]
+
+        [Test]
+        public void TestSendingSinglePrefixOnlyLookup()
+        {
+            var serializer = new FakeSerializer(new byte[0]);
+            var client = new Client(this.urlSender, serializer);
+
+            client.Send(new Lookup("1"));
+
+            Assert.AreEqual("http://localhost/?prefix=1&suggestions=10&geolocate=true&geolocate_precision=city", capturingSender.Request.GetUrl());
+        }
+
+        [Test]
+        public void TestSendingSingleFullyPopulatedLookup()
+        {
+            var serializer = new FakeSerializer(new byte[0]);
+            var client = new Client(this.urlSender, serializer);
+            var expectedURL = "http://localhost/?prefix=1&suggestions=2&city_filter=3&state_filter=4&prefer=5&geolocate=true&geolocate_precision=state";
+            var lookup = new Lookup();
+            lookup.Prefix = "1";
+            lookup.MaxSuggestions = 2;
+            lookup.AddCityFilter("3");
+            lookup.AddStateFilter("4");
+            lookup.AddPrefer("5");
+            lookup.GeolocateType = GeolocateType.STATE;
+
+            client.Send(lookup);
+
+            Assert.AreEqual(expectedURL, capturingSender.Request.GetUrl());
+        }
+
+        #endregion
+
+        #region [ Response Handling ]
+
+        [Test]
+        public void TestDeserializeCalledWithResponseBody()
+        {
+            var response = new Response(0, Encoding.ASCII.GetBytes("Hello, World!"));
+            var mockSender = new MockSender(response);
+            var sender = new URLPrefixSender("http://localhost/", mockSender);
+            var deserializer = new FakeDeserializer(new Result());
+            var client = new Client(sender, deserializer);
+
+            client.Send(new Lookup("1"));
+
+            Assert.AreEqual(response.Payload, deserializer.Payload);
+        }
+
+        [Test]
+        public void TestRejectNullLookup()
+        {
+            var serializer = new FakeSerializer(null);
+            var client = new Client(this.urlSender, serializer);
+
+            Assert.Throws<SmartyException>(() => client.Send(null));
+        }
+
+        [Test]
+        public void TestRejectNullPrefix()
+        {
+            var serializer = new FakeSerializer(null);
+            var client = new Client(this.urlSender, serializer);
+
+            Assert.Throws<SmartyException>(() => client.Send(new Lookup()));
+        }
+
+        [Test]
+        public void TestRejectEmptyPrefix()
+        {
+            var serializer = new FakeSerializer(null);
+            var client = new Client(this.urlSender, serializer);
+
+            Assert.Throws<SmartyException>(() => client.Send(new Lookup("")));
+        }
+
+
+        [Test]
+        public void TestResultCorrectlyAssignedToLookup()
+        {
+            var lookup = new Lookup("1");
+            var expectedResult = new Result();
+
+            var mockSender = new MockSender(new Response(0, Encoding.ASCII.GetBytes("{[]}")));
+            var sender = new URLPrefixSender("http://localhost/", mockSender);
+            var deserializer = new FakeDeserializer(expectedResult);
+            var client = new Client(sender, deserializer);
+
+            client.Send(lookup);
+
+            Assert.AreEqual(expectedResult.Suggestions, lookup.Result);
+        }
+
+    #endregion
+    }
+}
