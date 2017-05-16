f65fdeba7e1efffad91be89aba1ee34456235b1f
Neo Pimentel <neo@smartystreets.com>
2017-05-16 15:12:55 -0600

    Implemented International Metadata.

---
+++ src/sdk/InternationalStreetApi/Metadata.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+
+namespace SmartyStreets.InternationalStreetApi
+{
+    /// <summary>
+    /// See "https://smartystreets.com/docs/cloud/international-street-api#metadata"
+    /// </summary>
+    [DataContract]
+    public class Metadata
+    {
+        #region [ Fields ]
+
+        [DataMember(Name = "latitude")]
+        public double Latitude;
+
+        [DataMember(Name = "longitude")]
+        public double Longitude;
+
+        [DataMember(Name = "geocode_precision")]
+        public string GeocodePrecision;
+
+        [DataMember(Name = "max_geocode_precision")]
+        public string MaxGeocodePrecision;
+
+        #endregion
+    }
+}
