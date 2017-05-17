25f738c28905302056eae918a6ce0a4168ecf240
Whatcott, Michael <mdwhatcott@gmail.com>
2017-05-17 14:37:19 -0600

    Shorten long lines.

--- src/tests/StaticCredentialsTests.cs
+++ src/tests/StaticCredentialsTests.cs
@@ -8,13 +8,21 @@
 		[Test]
 		public void TestStandardCredentials()
 		{
-			AssertSignedRequest("f83280df-s83d-f82j-d829-kd02l9tis7ek", "S9Djk63k2Ilj67vN82Km", "https://us-street.api.smartystreets.com/street-address?auth-id=f83280df-s83d-f82j-d829-kd02l9tis7ek&auth-token=S9Djk63k2Ilj67vN82Km");
+			AssertSignedRequest(
+				"f83280df-s83d-f82j-d829-kd02l9tis7ek",
+				"S9Djk63k2Ilj67vN82Km",
+				"https://us-street.api.smartystreets.com/street-address?" +
+					"auth-id=f83280df-s83d-f82j-d829-kd02l9tis7ek&auth-token=S9Djk63k2Ilj67vN82Km");
 		}
 
 		[Test]
 		public void TestUrlEncoding()
 		{
-			AssertSignedRequest("as3$d8+56d9", "d8j#ds'dfe2", "https://us-street.api.smartystreets.com/street-address?auth-id=as3%24d8%2b56d9&auth-token=d8j%23ds%27dfe2");
+			AssertSignedRequest(
+				"as3$d8+56d9",
+				"d8j#ds'dfe2",
+				"https://us-street.api.smartystreets.com/street-address?" +
+					"auth-id=as3%24d8%2b56d9&auth-token=d8j%23ds%27dfe2");
 		}
 
 		private static void AssertSignedRequest(string id, string secret, string expected)
