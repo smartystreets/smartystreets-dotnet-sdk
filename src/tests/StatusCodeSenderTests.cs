addce8abc43b6956c7aaafa4475678bd663c93cd
Whatcott, Michael <mdwhatcott@gmail.com>
2017-05-17 14:32:26 -0600

    Consolidated status code sender assertions to a single test method.

--- src/tests/StatusCodeSenderTests.cs
+++ src/tests/StatusCodeSenderTests.cs
@@ -6,60 +6,23 @@
 	public class StatusCodeSenderTests
 	{
 		[Test]
-		public void Test200Response()
+		public void TestThrowsOnNon200Reponse()
 		{
-			var sender = new StatusCodeSender(new MockStatusCodeSender(200));
+			Assert.DoesNotThrow(() => Send(200));
 
-			var response = sender.Send(new Request());
-
-			Assert.AreEqual(200, response.StatusCode);
+			Assert.Throws<BadCredentialsException>(() => Send(401));
+			Assert.Throws<PaymentRequiredException>(() => Send(402));
+			Assert.Throws<RequestEntityTooLargeException>(() => Send(413));
+			Assert.Throws<BadRequestException>(() => Send(400));
+			Assert.Throws<TooManyRequestsException>(() => Send(429));
+			Assert.Throws<InternalServerErrorException>(() => Send(500));
+			Assert.Throws<ServiceUnavailableException>(() => Send(503));
 		}
 
-		[Test]
-		public void Test401ResponseThrowsBadCredentialsException()
+		private static void Send(int statusCode)
 		{
-			Assert.Throws<BadCredentialsException>(() => AssertSend(401));
-		}
-
-		[Test]
-		public void Test402ResponsePThrowsPaymentRequiredException()
-		{
-			Assert.Throws<PaymentRequiredException>(() => AssertSend(402));
-		}
-
-		[Test]
-		public void Test413ResponseThrowsRequestEntityTooLargeException()
-		{
-			Assert.Throws<RequestEntityTooLargeException>(() => AssertSend(413));
-		}
-
-		[Test]
-		public void Test400ResponseThrowsBadRequestException()
-		{
-			Assert.Throws<BadRequestException>(() => AssertSend(400));
-		}
-
-		[Test]
-		public void Test429ResponseThrowsTooManyRequestsException()
-		{
-			Assert.Throws<TooManyRequestsException>(() => AssertSend(429));
-		}
-
-		[Test]
-		public void Test500ResponseThrowsInternalServerErrorException()
-		{
-			Assert.Throws<InternalServerErrorException>(() => AssertSend(500));
-		}
-
-		[Test]
-		public void Test503ResponseThrowsServiceUnavailableException()
-		{
-			Assert.Throws<ServiceUnavailableException>(() => AssertSend(503));
-		}
-
-		private static void AssertSend(int statusCode)
-		{
-			var sender = new StatusCodeSender(new MockStatusCodeSender(statusCode));
+			var inner = new MockStatusCodeSender(statusCode);
+			var sender = new StatusCodeSender(inner);
 			sender.Send(new Request());
 		}
 	}
