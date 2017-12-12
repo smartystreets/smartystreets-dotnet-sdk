b5ddfcd92f0a34163ae8f43b76b416a8902400ed
Neo Pimentel <neo@smartystreets.com>
2017-12-12 10:29:12 -0700

    Clarified about environment variables in example.

--- src/examples/USStreetSingleAddressExample.cs
+++ src/examples/USStreetSingleAddressExample.cs
@@ -9,8 +9,13 @@
 	{
 		public static void Run()
 		{
-			var authId = Environment.GetEnvironmentVariable("SMARTY_AUTH_ID");
-			var authToken = Environment.GetEnvironmentVariable("SMARTY_AUTH_TOKEN");
+			var authId = "Your SmartyStreets Auth ID here";
+			var authToken = "Your SmartyStreets Auth Token here";
+			
+			// We recommend storing your keys in environment variables instead---it's safer!
+//			var authId = Environment.GetEnvironmentVariable("SMARTY_AUTH_ID");
+//			var authToken = Environment.GetEnvironmentVariable("SMARTY_AUTH_TOKEN");
+			
 			var client = new ClientBuilder(authId, authToken)
                                 //.ViaProxy("http://localhost:8080", "username", "password") // uncomment this line to point to the specified proxy.
                                 .BuildUsStreetApiClient();
