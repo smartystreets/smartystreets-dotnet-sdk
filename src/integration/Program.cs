1cd1d6f677051f1330a012766c946d5d0ac30ffc
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-04-13 06:57:18 -0600

    Renamed file; revised internal coding conventions.

---
+++ src/integration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IntegrationTests
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Tests.RunAllApiIntegrationTests();
+        }
+    }
+}
