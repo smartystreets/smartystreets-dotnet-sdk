85c5ca398f207e1b11381c3579060b5c2d435ce7
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2020-01-24 09:37:01 -0700

    Example of how to use SDK to point to an alternate installation of the API endpoints.
    This can be useful for things like stunnel or local installations of the APIs.

---
+++ src/examples/USStreetSingleAddressEndpointExample.cs
@@ -0,0 +1,77 @@
+﻿namespace Examples
+{
+	using System;
+	using System.IO;
+	using SmartyStreets;
+	using SmartyStreets.USStreetApi;
+
+	internal static class USStreetSingleAddressExample
+	{
+		public static void Run()
+		{
+			// var authId = "Your SmartyStreets Auth ID here";
+			// var authToken = "Your SmartyStreets Auth Token here";
+
+			// We recommend storing your keys in environment variables instead---it's safer!
+			var authId = Environment.GetEnvironmentVariable("SMARTY_AUTH_ID");
+			var authToken = Environment.GetEnvironmentVariable("SMARTY_AUTH_TOKEN");
+
+			var client = new ClientBuilder(authId, authToken)
+				// NOTE: this is how to point the SDK at an alternate installation
+				// for example, this might be used to connect through "stunnel" to handle things like TLSv1.2 encryption
+				.WithCustomBaseUrl("http://127.0.0.1:8080/street-address") // point to local installation
+				.BuildUsStreetApiClient();
+			
+			// Documentation for input fields can be found at:
+			// https://smartystreets.com/docs/us-street-api#input-fields
+
+			var lookup = new Lookup
+			{
+				InputId = "24601", // Optional ID from you system
+				Addressee = "John Doe",
+				Street = "1600 Amphitheatre Pkwy",
+				Street2 = "closet under the stairs",
+				Secondary = "APT 2",
+				Urbanization = "", // Only applies to Puerto Rico addresses
+				City = "Mountain View",
+				State = "CA",
+				ZipCode = "21229",
+				MaxCandidates = 3,
+				MatchStrategy = Lookup.INVALID // "invalid" is the most permissive match,
+                                               // this will always return at least one result even if the address is invalid.
+                                               // Refer to the documentation for additional MatchStrategy options.
+			};
+
+			try
+			{
+				client.Send(lookup);
+			}
+			catch (SmartyException ex)
+			{
+				Console.WriteLine(ex.Message);
+				Console.WriteLine(ex.StackTrace);
+			}
+			catch (IOException ex)
+			{
+				Console.WriteLine(ex.StackTrace);
+			}
+
+			var candidates = lookup.Result;
+
+			if (candidates.Count == 0)
+			{
+				Console.WriteLine("No candidates. This means the address is not valid.");
+				return;
+			}
+
+			var firstCandidate = candidates[0];
+
+			Console.WriteLine("Input ID: " + firstCandidate.InputId);
+			Console.WriteLine("Address is valid. (There is at least one candidate)\n");
+			Console.WriteLine("ZIP Code: " + firstCandidate.Components.ZipCode);
+			Console.WriteLine("County: " + firstCandidate.Metadata.CountyName);
+			Console.WriteLine("Latitude: " + firstCandidate.Metadata.Latitude);
+			Console.WriteLine("Longitude: " + firstCandidate.Metadata.Longitude);
+		}
+	}
+}
