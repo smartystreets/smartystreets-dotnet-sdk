8d27a1b590c285d917deec8fb303f6217897c9a9
jerry <jerry@smarty.com>
2023-01-25 11:22:21 -0700

    Added two more geolocateType strings

--- src/sdk/InternationalAutcompleteApi/GeolocateType.cs
+++ src/sdk/InternationalAutcompleteApi/GeolocateType.cs
@@ -2,6 +2,8 @@ namespace SmartyStreets.InternationalAutocompleteApi
 {
     public static class GeolocateType
     {
+        public const string ADMINAREA = "adminarea";
+        public const string LOCALITY = "locality";
         public const string POSTALCODE = "postalcode";
         public const string GEOCODES = "geocodes";
         public const string NONE = "";
