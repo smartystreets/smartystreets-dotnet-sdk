94f8d529cb74603b2bb50613bdd147b4c5ea5456
Landon Fackrell <landon@smarty.com>
2024-01-03 12:32:11 -0700

    Setup basic framework

---
+++ src/sdk/USEnrichmentApi/IUSEnrichmentClient.cs
@@ -0,0 +1,7 @@
+namespace SmartyStreets.USEnrichmentApi
+{
+    // marker interface for easy dependency injection and unit test mocking
+    public interface IUSEnrichmentClient : IClient<Lookup>
+    {
+    }
+}
