9bf86af37b09a6aa06d92b40056f1b62b9a6baa3
Landon Fackrell <landon@smarty.com>
2024-01-04 10:08:01 -0700

    Finish client

--- src/sdk/USEnrichmentApi/Client.cs
+++ src/sdk/USEnrichmentApi/Client.cs
@@ -13,5 +13,34 @@ namespace SmartyStreets.USEnrichmentApi
 			this.serializer = serializer;
 		}
 
+		public SmartyStreets.USEnrichmentApi.ResultTypes.Property.Financial.Result[] SendPropertyFinancialLookup(string smartyKey)
+		{
+			SmartyStreets.USEnrichmentApi.LookupTypes.Property.Financial.Lookup lookup = new SmartyStreets.USEnrichmentApi.LookupTypes.Property.Financial.Lookup(smartyKey);
+			Send(lookup);
+			return lookup.GetResults();
+		}
+
+		public SmartyStreets.USEnrichmentApi.ResultTypes.Property.Principal.Result[] SendPropertyPrincipalLookup(string smartyKey)
+		{
+			SmartyStreets.USEnrichmentApi.LookupTypes.Property.Principal.Lookup lookup = new SmartyStreets.USEnrichmentApi.LookupTypes.Property.Principal.Lookup(smartyKey);
+			Send(lookup);
+			return lookup.GetResults();
+		}
+
+		private void Send(Lookup lookup)
+		{
+			if (lookup == null || string.IsNullOrEmpty(lookup.SmartyKey))
+				throw new SmartyStreets.SmartyException("Client.Send() requires a Lookup with the 'smartyKey' field set");
+			Request request = BuildRequest(lookup);
+			Response response = this.sender.Send(request);
+			lookup.DeserializeAndSetResults(serializer, response.Payload);
+		}
+
+		private SmartyStreets.Request BuildRequest(SmartyStreets.USEnrichmentApi.LookupTypes.Lookup lookup)
+		{
+			SmartyStreets.Request request = new SmartyStreets.Request();
+			request.SetUrlPrefix("/" + lookup.SmartyKey + "/" + lookup.DatasetName + "/" + lookup.DataSubsetName);
+			return request;
+		}
     }
 }
