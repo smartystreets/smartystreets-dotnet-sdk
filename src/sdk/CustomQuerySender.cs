84fc32d1f07ce8fd33e05a993c9519abe4024bf2
Andrew Johnson <andrewjohnson0613@gmail.com>
2025-10-20 14:54:24 -0600

    Adding custom query option.

---
+++ src/sdk/CustomQuerySender.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SmartyStreets
+{
+    public class CustomQuerySender : ISender
+    {
+        private bool senderWasDisposed;
+        private readonly Dictionary<string, string> queries;
+        private readonly ISender inner;
+
+        public CustomQuerySender(Dictionary<string, string> queries, ISender inner)
+        {
+            this.queries = queries;
+            this.inner = inner;
+        }
+
+        public Response Send(Request request)
+        {
+            return SendAsync(request).GetAwaiter().GetResult();
+        }
+
+        public async Task<Response> SendAsync(Request request)
+        {
+            foreach (var query  in this.queries)
+            {
+                request.SetParameter(query.Key, query.Value);
+            }
+            return await this.inner.SendAsync(request);
+        }
+
+        public void Dispose()
+        {
+            if (!senderWasDisposed)
+            {
+                this.inner.Dispose();
+                this.senderWasDisposed = true;
+            }
+        }
+
+        public void EnableLogging()
+        {
+            this.inner.EnableLogging();
+        }
+    }
+}
