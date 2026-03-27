411a86f9ef04fc2e4d44e1a89b81773bac38a9ce
Andy Johnson <andrewjohnson0613@gmail.com>
2026-03-27 16:51:25 -0600

    Add WithSender() option with middleware   chain and conflict validation (#69)
    * Added wrapped http sender option for custom HTTP transport with middleware chain.
    
    * Changed withSender to wrap provided sender in the middleware chain.
    
    * Added conflict validation when withSender() is used with native-transport-only options.

--- src/tests/ClientBuilderTests.cs
+++ src/tests/ClientBuilderTests.cs
@@ -31,5 +31,46 @@ namespace SmartyStreets
 
             Assert.AreEqual("component-analysis,iana-timezone", GetCustomQueries(builder)["features"]);
         }
+
+        [Test]
+        public void TestWithSender_ThrowsWhenCombinedWithMaxTimeout()
+        {
+            var capturingSender = new RequestCapturingSender();
+            var builder = new ClientBuilder("test-id", "test-token")
+                .WithSender(capturingSender)
+                .WithMaxTimeout(System.TimeSpan.FromSeconds(5))
+                .WithSerializer(new FakeSerializer(null));
+
+            Assert.Throws<System.InvalidOperationException>(() => builder.BuildUsStreetApiClient());
+        }
+
+        [Test]
+        public void TestWithSender_ThrowsWhenCombinedWithProxy()
+        {
+            var capturingSender = new RequestCapturingSender();
+            var builder = new ClientBuilder("test-id", "test-token")
+                .WithSender(capturingSender)
+                .ViaProxy("http://localhost:8080", "", "")
+                .WithSerializer(new FakeSerializer(null));
+
+            Assert.Throws<System.InvalidOperationException>(() => builder.BuildUsStreetApiClient());
+        }
+
+        [Test]
+        public void TestWithSender_WrapsWithMiddlewareChain()
+        {
+            var capturingSender = new RequestCapturingSender();
+            var client = new ClientBuilder("test-id", "test-token")
+                .WithSender(capturingSender)
+                .WithSerializer(new FakeSerializer(null))
+                .BuildUsStreetApiClient();
+
+            client.Send(new USStreetApi.Lookup("1 Rosedale"));
+
+            var url = capturingSender.Request.GetUrl();
+            Assert.That(url, Does.Contain("us-street.api.smarty.com"));
+            Assert.That(url, Does.Contain("auth-id=test-id"));
+            Assert.That(url, Does.Contain("auth-token=test-token"));
+        }
     }
 }
